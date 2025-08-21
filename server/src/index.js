a8b372b9fa2c136e717167bc68c9bb5d6626f749
ezhil0017 <ezhil0017@gmail.com>
2025-08-21 14:18:36 +0530

    Implement dynamic routing for user orders with query and path parameters

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use('/come', (req, res) => {
-  res.send('Hello, World!');
-});
-app.use('/comeon', (req, res) => {
-  res.send('Hello, World!');
+//! dynamic routing using both query and path params
+//# http://localhost:3000/getuser/12345/orders?status=pending&limit=5
+app.get('/getuser/:userId/orders', (req, res) => {
+  console.log('path params: ', req.params);
+  console.log('query params: ', req.query);
+  res.send('dynamic query and path called successfully');
 });
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
